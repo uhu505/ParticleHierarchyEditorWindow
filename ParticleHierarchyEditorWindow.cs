8feb0fbb6a97fda80d00ca6fa70eb04bad7f6cf9
Uğur Yaman <yamanugur3373@gmail.com>
2023-05-29 19:06:38 +0300

    Update ParticleHierarchyEditorWindow.cs
    optimize and childonject feature

--- ParticleHierarchyEditorWindow.cs
+++ ParticleHierarchyEditorWindow.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
 using UnityEditor;
+using System.Collections.Generic;
 
 public class ParticleHierarchyEditorWindow : EditorWindow
 {
     private GameObject selectedObject;
     private bool isObjectSelected;
     private string projectName = "";
-    private string childObjectName = "";
+    private List<string> childObjectNames = new List<string>();
     private GameObject particleProperties;
     private bool hasParticleProperties;
+    private string debugWarning = "";
+    private bool showWarning;
 
     [MenuItem("Window/Particle Hierarchy Editor")]
     static void Init()
@@ -35,13 +38,30 @@ public class ParticleHierarchyEditorWindow : EditorWindow
             if (isObjectSelected)
             {
                 projectName = selectedObject.name;
+                UpdateChildObjectNames();
             }
             else
             {
                 projectName = "";
+                childObjectNames.Clear();
             }
 
             Repaint();
+        }
+    }
+
+    void UpdateChildObjectNames()
+    {
+        childObjectNames.Clear();
+
+        if (isObjectSelected)
+        {
+            Transform selectedTransform = selectedObject.transform;
+            for (int i = 0; i < selectedTransform.childCount; i++)
+            {
+                Transform childTransform = selectedTransform.GetChild(i);
+                childObjectNames.Add(childTransform.name);
+            }
         }
     }
 
@@ -63,66 +83,132 @@ public class ParticleHierarchyEditorWindow : EditorWindow
             EditorGUI.BeginDisabledGroup(true);
             EditorGUILayout.TextField("Project Name: ", projectName);
             EditorGUI.EndDisabledGroup();
-
-            childObjectName = EditorGUILayout.TextField("Child Object Name: ", childObjectName);
-
-            if (GUILayout.Button("Create"))
-            {
-                if (selectedObject != null && hasParticleProperties)
-                {
-                    if (!string.IsNullOrEmpty(childObjectName))
-                    {
-                        GameObject childObject = Instantiate(particleProperties, selectedObject.transform);
-                        childObject.name = childObjectName;
-
-                        Debug.Log("Child object created: " + childObjectName);
-                    }
-                }
-            }
         }
         else
         {
             projectName = EditorGUILayout.TextField("Project Name: ", projectName);
-            childObjectName = EditorGUILayout.TextField("Child Object Name: ", childObjectName);
+        }
 
-            if (GUILayout.Button("Create"))
+        EditorGUILayout.LabelField("Child Objects:");
+
+        // Child Object listesi için GUI çizimi
+        for (int i = 0; i < childObjectNames.Count; i++)
+        {
+            EditorGUILayout.BeginHorizontal();
+            childObjectNames[i] = EditorGUILayout.TextField("Name: ", childObjectNames[i]);
+            if (GUILayout.Button("Remove"))
             {
-                if (hasParticleProperties)
+                childObjectNames.RemoveAt(i);
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        // Yeni Child Object ekleme butonu
+        if (GUILayout.Button("Add Child Object"))
+        {
+            childObjectNames.Add("");
+        }
+
+        if (GUILayout.Button("Create"))
+        {
+            if (childObjectNames.Count == 0 && isObjectSelected)
+            {
+                showWarning = true;
+                debugWarning = "Please add at least one child object.";
+                EditorGUILayout.HelpBox(debugWarning, MessageType.Warning);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(projectName))
+            {
+                showWarning = true;
+                debugWarning = "Please fill in the project name.";
+                EditorGUILayout.HelpBox(debugWarning, MessageType.Warning);
+                return;
+            }
+
+            bool hasEmptyChildNames = false;
+            foreach (string childObjectName in childObjectNames)
+            {
+                if (string.IsNullOrEmpty(childObjectName))
                 {
-                    GameObject parentObject = Instantiate(particleProperties);
-                    parentObject.name = projectName;
-
-                    if (!string.IsNullOrEmpty(childObjectName))
-                    {
-                        GameObject childObject = Instantiate(particleProperties, parentObject.transform);
-                        childObject.name = childObjectName;
-
-                        Debug.Log("Child object created: " + childObjectName);
-                    }
-
-                    Debug.Log("Project object created: " + projectName);
+                    hasEmptyChildNames = true;
+                    break;
                 }
+            }
+
+            if (hasEmptyChildNames)
+            {
+                showWarning = true;
+                debugWarning = "Please fill in all child object names.";
+                EditorGUILayout.HelpBox(debugWarning, MessageType.Warning);
+                return;
+            }
+
+            if (hasParticleProperties)
+            {
+                if (isObjectSelected)
+                {
+                    CreateChildObjects(selectedObject.transform);
+                }
+                else
+                {
+                    CreateParentObject();
+                }
+
+                showWarning = false;
             }
         }
 
         if (GUILayout.Button("Clear"))
         {
-            selectedObject = null;
-            isObjectSelected = false;
-            projectName = "";
-            childObjectName = "";
-            particleProperties = null;
-            hasParticleProperties = false;
+            ClearFields();
         }
 
         if (particleProperties == null)
         {
             EditorGUILayout.HelpBox("Please assign a particle properties object.", MessageType.Warning);
-            selectedObject = null;
-            isObjectSelected = false;
-            projectName = "";
-            childObjectName = "";
+            ClearFields();
         }
 
+        if (showWarning)
+        {
+            EditorGUILayout.HelpBox(debugWarning, MessageType.Warning);
+        }
+    }
+
+    void CreateParentObject()
+    {
+        GameObject parentObject = Instantiate(particleProperties);
+        parentObject.name = projectName;
+
+        CreateChildObjects(parentObject.transform);
+
+        Debug.Log("Project object created: " + projectName);
+    }
+
+    void CreateChildObjects(Transform parentTransform)
+    {
+        foreach (string childObjectName in childObjectNames)
+        {
+            if (!string.IsNullOrEmpty(childObjectName))
+            {
+                GameObject childObject = Instantiate(particleProperties, parentTransform);
+                childObject.name = childObjectName;
+
+                Debug.Log("Child object created: " + childObjectName);
+            }
+        }
+    }
+
+    void ClearFields()
+    {
+        selectedObject = null;
+        isObjectSelected = false;
+        projectName = "";
+        childObjectNames.Clear();
+        particleProperties = null;
+        hasParticleProperties = false;
+        showWarning = false;
     }
 }
